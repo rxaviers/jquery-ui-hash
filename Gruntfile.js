16ae499bf2f3a9d49e03ac3452ffb8bde584943b
Rafael Xavier de Souza <rxaviers@gmail.com>
2013-09-05 14:36:17 -0300

    Add mocha testing metadata

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,28 @@ module.exports = function(grunt) {
 
 	"use strict";
 
+	var mountFolder = function (connect, path) {
+		return connect.static(require("path").resolve(path));
+	};
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
+		connect: {
+			options: {
+				port: 9001,
+				hostname: "localhost"
+			},
+			test: {
+				options: {
+					middleware: function (connect) {
+						return [
+							mountFolder(connect, "."),
+							mountFolder(connect, "test")
+						];
+					}
+				}
+			}
+		},
 		jshint: {
 			source: {
 				src: ["src/*.js"],
@@ -16,13 +36,31 @@ module.exports = function(grunt) {
 				options: {
 					jshintrc: ".jshintrc"
 				}
+			},
+			test: {
+				src: [ "test/**/*.js" ],
+				options: {
+					jshintrc: "test/.jshintrc"
+				}
+			}
+		},
+		mocha: {
+			all: {
+				options: {
+					urls: [ "http://localhost:<%= connect.options.port %>/index.html" ]
+				}
 			}
 		}
 	});
 
 	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
-	grunt.registerTask("default", ["jshint"]);
+	grunt.registerTask("test", [
+		"connect:test",
+		"mocha"
+	]);
+
+	grunt.registerTask("default", ["jshint", "test"]);
 
 };
 
